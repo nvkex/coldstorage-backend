e7e8eb3a80bdc4a4ad84f5e1974fc1349307fbb9
nvkex <scrbwrm@gmail.com>
2021-03-04 19:20:15 +0530

    categories data

---
+++ data/categories.js
@@ -0,0 +1,23 @@
+exports.categories= [
+    { 
+        title : "Documents"
+    },
+    { 
+        title : "Images"
+    },
+    { 
+        title : "Video"
+    },
+    { 
+        title : "Audio"
+    },
+    { 
+        title : "Music"
+    },
+    { 
+        title : "Books"
+    },
+    { 
+        title : "Web Pages"
+    },
+]
